c56117bb10713036332cc5742ab8bffb951fe37f
Privex <privexchat@gmail.com>
2026-06-09 20:37:09 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -882,7 +882,6 @@ class SecurityHeadersMiddleware(BaseHTTPMiddleware):
         response = await call_next(request)
         response.headers["Server"] = ""
         response.headers["X-Content-Type-Options"] = "nosniff"
-        response.headers["X-Frame-Options"] = "DENY"
         response.headers["Referrer-Policy"] = "strict-origin-when-cross-origin"
         response.headers["Permissions-Policy"] = "geolocation=(), camera=(), microphone=()"
         return response
